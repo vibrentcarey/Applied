d56528bdd58f431ba6962d27e25f5af7ceb06708
Brent Carey <brentbcarey@gmail.com>
2022-02-17 13:06:12 -0800

    Add select to table

--- pages/jobs.tsx
+++ pages/jobs.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import {SiIndeed, SiLinkedin} from 'react-icons/si'
+import { SiIndeed, SiLinkedin } from "react-icons/si";
 const Jobs = () => {
   return (
     <div className="overflow-x-auto">
-      <table className="table table-compact w-full">
+      <table className="table table-compact w-full ">
         <thead>
           <tr>
             <th></th>
@@ -11,6 +11,7 @@ const Jobs = () => {
             <th>company</th>
             <th>Platform</th>
             <th>Date Applied</th>
+            <th>Status</th>
           </tr>
         </thead>
         <tbody>
@@ -18,16 +19,38 @@ const Jobs = () => {
             <th>1</th>
             <td>Cy Ganderton</td>
             <td>Quality Control Specialist</td>
-            <td className="text-primary"><SiIndeed/></td>
+            <td className="text-primary">
+              <SiIndeed />
+            </td>
             <td>12/16/2020</td>
+            <td>
+              <select className="select select-xs max-w-xs select-primary">
+                <option disabled selected>
+                  Status
+                </option>
+                <option>Pending</option>
+                <option>Not Accepted</option>
+                <option>Interview</option>
+              </select>
+            </td>
           </tr>
           <tr>
             <th>2</th>
             <td>Hart Hagerty</td>
             <td>Desktop Support Technician</td>
-            <td className="text-primary"><SiLinkedin/></td>
+            <td className="text-primary">
+              <SiLinkedin />
+            </td>
             <td>12/5/2020</td>
             <td>
+              <select className="select select-xs max-w-xs select-primary">
+                <option disabled selected>
+                  Status
+                </option>
+                <option>Pending</option>
+                <option>Not Accepted</option>
+                <option>Interview</option>
+              </select>
             </td>
           </tr>
         </tbody>
@@ -38,6 +61,7 @@ const Jobs = () => {
             <th>company</th>
             <th>Platform</th>
             <th>Date Applied</th>
+            <th>Status</th>
           </tr>
         </tfoot>
       </table>
