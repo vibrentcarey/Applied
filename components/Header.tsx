926dc42f1795d3c4c12f47507e411e14cb38c998
Brent Carey <brentbcarey@gmail.com>
2022-02-17 12:48:07 -0800

    Add table component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,17 @@
 import React from "react";
+import {MdLibraryAdd, MdTableChart} from 'react-icons/md'
 
 const Header = () => {
   return (
     <div className="navbar bg-primary shadow-xl rounded-b-sm">
-      <div className="navbar-start"></div>
+      <div className="navbar-start">
+      <a className="btn btn-ghost normal-case text-xl">Applied</a>
+      </div>
       <div className="navbar-center">
-        <a className="btn btn-ghost normal-case text-xl">Applied</a>
       </div>
       <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle"></button>
-        <button className="btn btn-ghost btn-circle">
-          <div className="indicator"></div>
+        <button className="btn btn-ghost text-2xl"><MdLibraryAdd/></button>
+        <button className="btn btn-ghost text-2xl"><MdTableChart/>
         </button>
       </div>
     </div>
---
+++ pages/jobs.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {SiIndeed, SiLinkedin} from 'react-icons/si'
+const Jobs = () => {
+  return (
+    <div className="overflow-x-auto">
+      <table className="table table-compact w-full">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Job</th>
+            <th>company</th>
+            <th>Platform</th>
+            <th>Date Applied</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>1</th>
+            <td>Cy Ganderton</td>
+            <td>Quality Control Specialist</td>
+            <td className="text-primary"><SiIndeed/></td>
+            <td>12/16/2020</td>
+          </tr>
+          <tr>
+            <th>2</th>
+            <td>Hart Hagerty</td>
+            <td>Desktop Support Technician</td>
+            <td className="text-primary"><SiLinkedin/></td>
+            <td>12/5/2020</td>
+            <td>
+            </td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <th></th>
+            <th>Job</th>
+            <th>company</th>
+            <th>Platform</th>
+            <th>Date Applied</th>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+};
+
+export default Jobs;
