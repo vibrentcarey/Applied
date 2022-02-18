029b99f18de68dcc8dec15e0f84f72bff6efab1a
Brent Carey <brentbcarey@gmail.com>
2022-02-17 16:19:02 -0800

    Setup front end session

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,32 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Hero from '../components/Hero'
+import { getSession } from "next-auth/client";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-export default function Home() {
+
+export default function Home({ session }) {
+  const router = useRouter()
+  useEffect(() => {
+    if (session) {
+    } else {
+      router.replace("/auth");
+    }
+  }, [session]);
   return (
 
     <main>
       <Hero />
     </main>
   )
+}
+
+
+export async function getServerSideProps(ctx) {
+  return {
+    props: {
+      session: await getSession(ctx),
+    },
+  };
 }
