c974ba22437ad8515bdfb36a05a921acce44e626
Brent Carey <brentbcarey@gmail.com>
2022-02-19 16:32:22 -0800

    Change hero copy

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <div className="max-w-lg">
           <h1 className="font-bold text-primary">Why Applied?</h1>
           <p className="">
-            Applying to jobs is stressful enough without having to keep track of all your applications.  Applied does the work of tracking of all your job applications across platforms, in one convenient place. 
+            Applying to jobs is stressful enough without having to keep track of all your applications.  Applied takes care of tracking all of your job applications across platforms in one convenient place, so you can focus on getting hired 💰.
           </p>
           <Link href="/add" passHref>
             <button className="btn btn-primary">Get Started</button>
